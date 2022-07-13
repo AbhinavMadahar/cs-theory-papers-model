27c72d7fcbf62b45745c94808db72cb1e35b4ece
Abhinav Madahar <abhinavmadahar@gmail.com>
2022-07-12 22:10:41 -0400

    Add train method

---
+++ model/train.py
@@ -0,0 +1,115 @@
+import math
+import random
+import torch
+import time
+
+from model.model import Encoder, Decoder
+from torch import optim, nn
+from typing import Iterable
+from model.vocabulary import EOS_token, SOS_token, tensor_from_sentence
+from numbers import Number
+from util.plot import show_plot
+
+
+MAX_LENGTH = 10
+teacher_forcing_ratio = 0.5
+
+def train_one_iteration(
+        encoder: Encoder, decoder: Decoder,
+        sequence: torch.Tensor,
+        encoder_optimizer, decoder_optimizer,
+        criterion,
+        device: torch.device,
+        max_length=MAX_LENGTH) -> float:
+    encoder_hidden = encoder.init_hidden()
+
+    encoder_optimizer.zero_grad()
+    decoder_optimizer.zero_grad()
+
+    sequence_length = sequence.size(0)
+
+    encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
+
+    loss = 0
+
+    for t in range(sequence_length):
+        encoder_output, encoder_hidden = encoder(sequence[t], encoder_hidden)
+        encoder_outputs[t] = encoder_output[0, 0]
+    
+    decoder_input = torch.tensor([[SOS_token]], device=device)
+    
+    decoder_hidden = encoder_hidden
+
+    use_teacher_forcing = random.random() < teacher_forcing_ratio
+
+    if use_teacher_forcing:
+        for t in range(sequence_length):
+            decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
+            loss += criterion(decoder_output, sequence[t])
+            decoder_input = sequence[t] 
+    else:
+        for t in range(sequence_length):
+            decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
+            _, topi = decoder_output.topk(1)
+            decoder_input = topi.squeeze().detach()
+
+            loss += criterion(decoder_output, sequence[t])
+            if decoder_input.item() == EOS_token:
+                break
+    
+    loss.backward()
+
+    encoder_optimizer.step()
+    decoder_optimizer.step()
+
+    return loss.item() / sequence_length
+
+
+def as_minutes(seconds: Number) -> str:
+    minutes = math.floor(seconds / 60)
+    seconds -= minutes * 60
+    return '%dm %ds' % (minutes, seconds)
+
+
+def time_since(since, percent) -> str:
+    now = time.time()
+    s = now - since
+    es = s / (percent)
+    rs = es - s
+    return '%s (- %s)' % (as_minutes(s), as_minutes(rs))
+
+
+def train(encoder: Encoder, decoder: Decoder,
+          sequences: Iterable[torch.Tensor],
+          print_every=1000, plot_every=100,
+          learning_rate=0.01,
+          iterations=100) -> None:
+    start = time.time()
+    plot_losses = []
+    print_loss_total = 0
+    plot_loss_total = 0
+
+    encoder_optimizer = optim.SGD(encoder.parameters(), lr=learning_rate)
+    decoder_optimizer = optim.SGD(decoder.parameters(), lr=learning_rate)
+
+    criterion = nn.NLLLoss()
+
+    for iteration, sequence in enumerate(sequences, start=1):
+        loss = train_one_iteration(encoder, decoder, sequence, encoder_optimizer, decoder_optimizer, criterion)
+        print_loss_total += loss
+        plot_loss_total += loss
+
+        if iteration % print_every == 0:
+            print_loss_avg = print_loss_total / print_every
+            print_loss_total = 0
+            print('%s (%d %d%%) %.4f' % (time_since(start, iteration / iterations),
+                                         iteration,
+                                         iteration / iterations * 100,
+                                         print_loss_avg))
+        
+        if iteration % plot_every == 0:
+            plot_loss_avg = plot_loss_total / plot_every
+            plot_losses.append(plot_loss_avg)
+            plot_loss_total = 0
+    
+    show_plot(plot_losses)
