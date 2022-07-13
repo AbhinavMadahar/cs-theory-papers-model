813dcb288d43989b335834be99e690cce9eaa983
Abhinav Madahar <abhinavmadahar@gmail.com>
2022-07-12 22:32:29 -0400

    Add vocabulary

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from model.model import Encoder, Decoder
 from model.train import train
+from model.vocabulary import Vocabulary
 
 abstracts_file_name = 'data/abstracts.txt'
 
 if __name__ == '__main__':
+    vocabulary = Vocabulary()
+
     with open(abstracts_file_name, 'r') as abstracts_file:
         abstracts = []
         abstract_lines = []
@@ -16,6 +19,8 @@ if __name__ == '__main__':
                     abstract = abstract.lower()
                     abstract = [word for word in abstract.split(' ') if all('a' <= char <= 'z' for char in word)]
                     abstracts.append(abstract)
+                    for word in abstract:
+                        vocabulary.add_word(word)
                 abstract_lines = []
             else:
                 abstract_lines.append(line)
--- model/vocabulary.py
+++ model/vocabulary.py
@@ -9,17 +9,17 @@ EOS_token = 1
 
 
 class Vocabulary:
-    def __init__(self):
+    def __init__(self) -> None:
         self.word2index = {}
         self.word2count = {}
         self.index2word = {0: "SOS", 1: "EOS"}
         self.n_words = 2  # Count SOS and EOS
 
-    def addSentence(self, sentence: str) -> None:
+    def add_sentence(self, sentence: str) -> None:
         for word in sentence.split(' '):
-            self.addWord(word)
+            self.add_word(word)
 
-    def addWord(self, word: str) -> None:
+    def add_word(self, word: str) -> None:
         if word not in self.word2index:
             self.word2index[word] = self.n_words
             self.word2count[word] = 1
